e05a13693e67e17732a8105fe48d6523b8593687
Warspoot <106011484+Warspoot@users.noreply.github.com>
2023-05-23 07:15:49 +0930

    Update

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 OPENAI_API_KEY = "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"
-Koblod_LINK = "https://frequent-third-identifying-february.trycloudflare.com/"
+Koblod_LINK = "https://ca-feb-gathered-hour.trycloudflare.com/"
